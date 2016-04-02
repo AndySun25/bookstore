01d6d40b60f0f0a56fbdd7c772ee77c6a6929277
Andy Sun <andy_sun_sha@hotmail.com>
2016-04-02 18:19:02 +0200

    Renamed main

--- src/Run.java
+++ src/Run.java
@@ -1,11 +1,11 @@
 import frontend.StoreFront;
 import store.Store;
 
-public class main {
+public class Run {
     static String source_url = "http://www.contribe.se/bookstoredata/bookstoredata.txt";
 
     public static void main(String[] args) {
         Store store = new Store(source_url);
-        StoreFront front = new StoreFront(store);
+        new StoreFront(store);
     }
 }
